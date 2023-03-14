4ec693eccaf6f5a17bdf612207f7caf66d67d47c
Ayush <iamayushagrawal2002@gmail.com>
2023-03-14 20:37:58 +0530

    Committed

---
+++ Sorting/Insertionsort.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+void sortarray(int arr[],int n){
+    for(int i=1; i<n; i++){
+        int temp= arr[i];
+        int j=i-1;
+        for(j; j>=0; j--){
+            if(arr[j]>temp){
+                arr[j+1]=arr[j];
+            }
+            else{
+                break;
+            }
+        }
+        arr[j+1]=temp;
+    }
+}
+void print(int arr[],int n){
+
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+int main(){
+  int n;
+  cin>>n;
+  int arr[n];
+  for(int i=0; i<n; i++){
+    cin>>arr[i];
+  }
+  sortarray(arr,n);
+  print(arr,n);
+
+
+}
--- Sorting/Selectionsort.cpp
+++ Sorting/Selectionsort.cpp
@@ -1,12 +1,36 @@
 #include<iostream>
 using namespace std;
-void sortarray(vector<int>& arr,int n){
+void sortarray(int arr[],int n){
+      for(int i=0; i<n-1; i++){
+        int minindex=i;
+        for(int j=(i+1); j<n; j++){
+          if(arr[j] < arr[minindex]){
+            minindex = j;
+          }
+          
+        }   
+        
+        swap(arr[minindex],arr[i]);
+        
+      }
+      
+}
+void print(int arr[],int n){
 
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
 }
 int main(){
   int n;
   cin>>n;
   int arr[n];
+  for(int i=0; i<n; i++){
+    cin>>arr[i];
+  }
+  sortarray(arr,n);
+  print(arr,n);
 
 
 }
